71d7b2b3a18491221a7f5e493ccdc9695d5affef
Peter Teoh <htmldeveloper@gmail.com>
2012-12-25 22:28:30 +0800

    new file:   src/epitrochoidv1.py new file:   src/epitrochoidv2.py new file:   src/epitrochoidv3.py

---
+++ src/epitrochoidv3.py
@@ -0,0 +1,31 @@
+
+import math
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor('lightblue')
+PI=3.14
+
+fred = turtle.Turtle()
+fred.speed(99999)
+
+#### d==r means epicycloid
+
+def epitrochoid(R, r, d, nos_cycle):
+    n=72
+    angle=2*PI/n
+    fred.penup()
+    status=9999
+    for i in range(nos_cycle*n):
+	   beta = i * angle 
+	   x = (R+r)*math.cos(beta) - d*math.cos((R+r)*beta/r)
+	   y = (R+r)*math.sin(beta) - d*math.sin((R+r)*beta/r)
+           fred.goto(x,y)
+	   if (status==9999):
+		fred.pendown()
+		status=1
+
+
+epitrochoid(10, 50, 50, 5)
+
+wn.exitonclick()
