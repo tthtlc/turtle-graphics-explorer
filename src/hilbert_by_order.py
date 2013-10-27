cf9a0cdfd435f699d24b4f367cb48308f746c2b3
Peter Teoh <htmldeveloper@gmail.com>
2013-10-27 10:16:34 +0800

    new file:   src/hilbert_by_distance.py new file:   src/hilbert_by_order.py

---
+++ src/hilbert_by_order.py
@@ -0,0 +1,63 @@
+
+#### this program contrast with hilbert.py
+#### the approach is via "order"
+#### the forwarding distance is fixed.
+
+import turtle as t
+import sys
+
+p = t.Pen()
+##p.reset()
+p.speed(0)
+global distance
+
+distance=999
+
+### A ==== - B F + A F A + F B -
+### X=     -YF+XFX+FY-
+def a(order):
+	global distance
+	if (order >= 1):
+		p.left(90) 
+		b(order-1)
+		p.forward(distance) 
+		p.right(90) 
+		a(order-1)
+		p.forward(distance) 
+		a(order-1)
+		p.right(90) 
+		p.forward(distance) 
+		b(order-1)
+		p.left(90) 
+
+### B ==== + A F - B F B - F A +
+### Y     =+XF-YFY-FX+
+def b(order):
+	global distance
+	if (order >= 1):
+		p.right(90) 
+		a(order-1)
+		p.forward(distance) 
+		p.left(90) 
+		b(order-1)
+		p.forward(distance) 
+		b(order-1)
+		p.left(90) 
+		p.forward(distance) 
+		a(order-1)
+		p.right(90) 
+
+if len(sys.argv) != 2:
+        print "Usage: "+sys.argv[0]+" order"
+        sys.exit(1)
+
+order = int(sys.argv[1])
+distance=int(80/order)
+
+p.penup()
+p.setx(-distance*(2**(order-1)))
+p.sety(-distance*(2**(order-1)))
+p.pendown()
+a(order)
+raw_input()
+
